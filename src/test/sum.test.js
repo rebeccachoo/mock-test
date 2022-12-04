7deee696319b2cbb7c44ad1d1ae6193b6b468142
Rebecca <rnwldms@gmail.com>
2022-12-03 22:34:39 -0600

    test added

---
+++ src/test/sum.test.js
@@ -0,0 +1,17 @@
+const sum = require("../sum");
+
+test("adds 1 + 2 to equal 3", () => {
+  expect(sum(1, 2)).toBe(3);
+});
+test("two plus two is four", () => {
+  expect(2 + 2).toBe(4);
+});
+test("2 + 2 = 4", () => {
+  expect(2 + 2).toBe(4);
+});
+
+test("object assignment", () => {
+  const data = { one: 1 };
+  data["two"] = 2;
+  expect(data).toEqual({ one: 1, two: 2 });
+});
