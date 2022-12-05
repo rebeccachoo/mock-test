3ecc20e90f51f3d768114499af1109dc8a7f6a94
Rebecca <rnwldms@gmail.com>
2022-12-05 09:56:53 -0600

    Error Fixed

--- src/test/async.test.js
+++ src/test/async.test.js
@@ -1,7 +1,16 @@
 const fetchData = require("../async");
 
-test("", () => {
-  return fetchData().then((data) => {
-    expect(data).toEqual({ item: "Milk", price: 200 });
+describe("Async", () => {
+  it("", () => {
+    return fetchData().then((data) => {
+      expect(data).toEqual({ item: "Milk", price: 200 });
+    });
+  });
+
+  it("the data is peanut butter", async () => {
+    await expect(fetchData()).resolves.toEqual({
+      item: "Milk",
+      price: 200,
+    });
   });
 });
